fdd3a20c88a8b517fb3348d2c2bd0696cf78aee6
Gregtstu <boltnev.grisha@yandex.ru>
2022-11-07 13:31:55 +0300

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SummaryPageComponent} from "./pages/summary-page/summary-page.component";
+import {ListPageComponent} from "./pages/list-page/list-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'summary', pathMatch:'full'},
+  {path:'summary', component:SummaryPageComponent},
+  {path:'list', component:ListPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { SummaryPageComponent } from './pages/summary-page/summary-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListPageComponent,
+    SummaryPageComponent
   ],
   imports: [
     BrowserModule,
